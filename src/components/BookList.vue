9f4d29328b3d3c8dd9ce2dcf9488e8db702cebda
ruthmashriki <61411071+ruthmashriki@users.noreply.github.com>
2020-02-24 13:07:44 +0200

    Completed module

---
+++ src/components/BookList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>  
+    <H1> {{title}} </H1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookList',
+  data() {
+      return {
+        title: "All Books"
+      }
+
+  },
+};
+</script>
+
+<style>
+h1, h2 {
+    font-weight: normal;
+}
+</style>

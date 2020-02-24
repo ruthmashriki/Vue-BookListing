60962c9affde8c778946a34203fbd68a1f7a5649
ruthmashriki <61411071+ruthmashriki@users.noreply.github.com>
2020-02-24 13:32:03 +0200

    Completed module

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>  
     <H1> {{title}} </H1>
+    <li>
+        <ul v-for="book in books"> {{book.title}}</ul>
+    </li>
   </div>
 </template>
 
@@ -9,7 +12,12 @@ export default {
   name: 'BookList',
   data() {
       return {
-        title: "All Books"
+        title: "All Books",
+        books: [
+    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+    {title: 'American Gods', author: 'Neil Gaiman'},
+    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+]
       }
 
   },
@@ -20,4 +28,14 @@ export default {
 h1, h2 {
     font-weight: normal;
 }
+li
+{list-style-type: none;
+padding: 0;
+}
+
+ul{
+    display: block;
+    margin: 0 10px;
+}
+
 </style>
